0d69e859e5469b20d929190cf0b946fbad32d55e
Austin Meyers <meyersaustin22@gmail.com>
2015-10-18 01:37:57 -0700

    Getting styles in place

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var CommentBox = require('./friendslist.js');
+var Footer = require('./footer.js');
 
 var App = React.createClass({
     render: function() {
         return(
             <div>
                 <CommentBox user="Sam"/>
+                <Footer />
             </div>
         );
     }
--- front_end/src/friendslist.js
+++ front_end/src/friendslist.js
@@ -4,7 +4,7 @@ var CommentBox = React.createClass({
 	render() {
 		return (
 			<div className="commentBox">
-				Hello, world! I am a CommentBox.
+				Hello, world! I am a CommentBox
 				{this.props.user}
 			</div>
 		);
