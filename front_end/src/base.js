163d7e42293ead2ac57a6aa948804912f812ae86
Austin Meyers <meyersaustin22@gmail.com>
2015-10-18 04:17:58 -0700

    Removed bad transition code

--- front_end/src/base.js
+++ front_end/src/base.js
@@ -1,13 +1,10 @@
 var React = require('react');
-var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var Base= React.createClass({
 	render() {
 		return (
 			<div className="base">
-				<ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={500} transitionLeaveTimeout={300} >
 					<p>Base</p>
-		        </ReactCSSTransitionGroup>
 			</div>
 		);
 	}
